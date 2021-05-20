ad2b8834ea30431f592abc9f3cfa4bd9a6948387
Fabricio <fabriciosperafico@outlook.com>
2021-05-19 22:52:55 -0300

    changes

--- MyApplication/app/src/main/java/com/example/myapplication/ui/main/AddNewTask.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/main/AddNewTask.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AddNewTask extends Fragment {
 
@@ -17,10 +19,26 @@ public class AddNewTask extends Fragment {
         return new AddNewTask();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button = view.findViewById(R.id.taskRemember);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_task, container, false);
+
         return view;
     }
 }
